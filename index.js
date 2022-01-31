aec0b78a1a58ac6a14a3b1fbbd456ca03d511ed3
Mike Starr <imhs@protonmail.com>
2022-01-31 07:04:36 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(name) {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(name) {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(name) {
+    return this.sex === "male"
+      ? `It's me! ${this.name}, the parrot!`
+      : `${this.name} says squawk!`;
+  }
+}
+
+const birdz = new Bird("mike", "male");
+console.log(birdz);
+console.log(birdz.speak("mike"));
